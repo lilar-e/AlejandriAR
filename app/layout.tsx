c675a65f30318e7345dcfdb760c014f6811d42a6
Leo <cardosoleonel152@gmail.com>
2025-01-13 11:41:33 -0300

    SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
             <Navigation />
           </div>
         </header>
+        <SpeedInsights />
         <main>{children}</main>
       </body>
     </html>
